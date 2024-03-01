478c5cd94e9cfcf730cac8ddf01899183df518de
osipovark <osipovarkonst@gmail.com>
2024-03-01 21:44:18 +0300

    second commit

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,6 +1,6 @@
 function useGetUserInfo() {
   const { name, profilePicture, userID, isAuth } = JSON.parse(
-    localStorage.getItem("auth")
+    localStorage.getItem("auth") || {}
   );
 
   return { name, profilePicture, userID, isAuth };
